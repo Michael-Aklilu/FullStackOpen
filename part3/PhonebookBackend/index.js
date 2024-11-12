9904a35364d4123dcddc479b1976cb01897a95db
Michael Aklilu <michaelaklilu@gmail.com>
2024-11-12 09:09:47 +0300

    Completed phonebook exercises

--- part3/PhonebookBackend/index.js
+++ part3/PhonebookBackend/index.js
@@ -1,30 +1,31 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const path = require("path");
-require("dotenv").config();
-const Person = require("./models/person");
+/* eslint-disable @stylistic/js/semi */
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+const Person = require('./models/person');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static("dist"));
-morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(express.static('dist'));
+morgan.token('body', (req) => JSON.stringify(req.body));
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
 app.use((req, res, next) => {
-  if (req.path.startsWith("/")) {
+  if (req.path.startsWith('/')) {
     console.log(`Static file requested: ${req.path}`);
   }
   next();
 });
 
-app.get("/api/persons", (request, response, next) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then((person) => {
       if (person) {
@@ -36,18 +37,17 @@ app.get("/api/persons", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.get("/api/persons/info", (request, response) => {
+app.get('/api/persons/info', (request, response) => {
   const now = new Date();
   Person.find({}).then((result) => {
     response.send(
-      `<p>Phonebook has info for ${
-        result.length
+      `<p>Phonebook has info for ${result.length
       } people </p> <p>${now.toString()}</p>`
     );
   });
 });
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((result) => {
       if (result) {
@@ -59,23 +59,23 @@ app.get("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(() => response.status(204).end())
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body;
 
-  if (body.name === "")
+  if (body.name === '')
     return response.status(400).json({
-      error: "name missing",
+      error: 'name missing',
     });
 
-  if (body.number === "")
+  if (body.number === '')
     return response.status(400).json({
-      error: "number missing",
+      error: 'number missing',
     });
 
   Person.findOne({ name: body.name }).then((p) => {
@@ -94,27 +94,20 @@ app.post("/api/persons", (request, response) => {
       });
     }
   });
-  /*const notUnique = person.some((p) => p.name === body.name);
-
-  if (notUnique)
-    return response.status(409).json({
-      error: "name must be unique",
-    });
-  */
 });
 
-app.get("/", (request, response) => {
-  response.sendFile(path.join(__dirname, "dist", "index.html"));
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
-  if (error.name === "NotFound") {
-    return response.status(404).send({ error: "Not found" });
+  if (error.name === 'NotFound') {
+    return response.status(404).send({ error: 'Not found' });
   }
-  if (error.name === "CastError") {
+  if (error.name === 'CastError') {
     return response.status(400).send({
-      error: "Malformatted id",
+      error: 'Malformatted id',
     });
   }
   next(error);
